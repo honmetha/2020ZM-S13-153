489e4e5c544f91d6a43f07faf71b0443e43f10af
Hon <dennis.mejen@gmail.com>
2020-04-20 22:26:08 +0700

    Remove files, add card component and install tachyons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
